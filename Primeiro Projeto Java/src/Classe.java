b52793f88ecc3715d736eac0a05b961803e2a5de
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-05-13 21:44:59 -0300

    Update Classe

---
+++ Primeiro Projeto Java/src/Classe.java
@@ -0,0 +1,19 @@
+public class Classe { /*CLASSE*/
+
+public static void main (String [] args) { /*MÉTODO*/
+
+    /*PRINT*/
+
+    System.out.print ( "Olá!");
+
+    /*VARIÁVEIS*/
+
+    int ano = 2021; /*Definindo variável */
+    ano = 2024; /*Alterando variável */
+    final String BR = "Brasil"; /*Variável não deve ser alterada */
+
+    
+
+}
+
+}
