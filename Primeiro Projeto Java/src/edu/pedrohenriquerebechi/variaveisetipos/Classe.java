2111eea459322f79f01a4ad44db04dffc480cce9
Pedro Henrique Rebechi <130265807+PedroHenriqueRebechi@users.noreply.github.com>
2024-05-17 09:43:11 -0300

    Atualização Tipos primitivos

--- Primeiro Projeto Java/src/edu/pedrohenriquerebechi/variaveisetipos/Classe.java
+++
@@ -1,7 +0,0 @@
-package edu.pedrohenriquerebechi.variaveisetipos;
-
-public class Classe {
-    public static void main (Strings [] args){
-        System.out.print("Oi");
-    }
-}
---
+++ Primeiro Projeto Java/src/edu/pedrohenriquerebechi/variaveisetipos/TiposVariaveis.java
@@ -0,0 +1,12 @@
+package edu.pedrohenriquerebechi.variaveisetipos;
+
+public class TiposVariaveis {
+    // Tipos primitivos
+
+    int numeroPequeno = 20; // Números pequenos inteiros
+    long numeroGrande = 2394209393993939393939939393993939393; // Números grandes inteiros
+
+    double decimal = 23.543; // Números decimais
+
+
+}
