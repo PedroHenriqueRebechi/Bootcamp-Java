c9dcd68d13f9dd43657292fc0ae6316bf2da540d
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-05-23 22:19:19 -0300

    Atualização - Ternários e Relacionais

--- Primeiro Projeto Java/src/edu/pedrohenriquerebechi/operadores/Operadores.java
+++ Primeiro Projeto Java/src/edu/pedrohenriquerebechi/operadores/Operadores.java
@@ -31,6 +31,49 @@ public class Operadores {
 
         boolean variavel = true;
         System.out.println("Inverte valor: " + !variavel);
+
+        // TERNÁRIO: Resumir  "?:"
+
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ?"Verdadeiro" :"Falso"; //IF e ELSE resumido
+        System.out.println(resultado);
+
+        // RELACIONAIS: Avaliam a relação entre duas variaveis ou expressões
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("Numero1 é igual a numero2? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("Numero1 é diferente do numero2? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("Numero1 é maior que numero2? " + simNao);
+
+        // Para objetos
+
+        String nomeUm = "Pedro";
+        String nomeDois = "Pedro";
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+
+
+
+
+
+         
+
+
         
     }
 }
