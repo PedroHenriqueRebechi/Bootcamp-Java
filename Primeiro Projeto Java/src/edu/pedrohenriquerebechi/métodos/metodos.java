7e02611ad7220f1514c6fa723cbfdda6a342b5ce
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-05-27 22:43:14 -0300

    Introdução aos Métodos

---
+++ Primeiro Projeto Java/src/edu/pedrohenriquerebechi/métodos/metodos.java
@@ -0,0 +1,16 @@
+package edu.pedrohenriquerebechi.métodos;
+
+public class metodos {
+    public double somar (int num1, int num2) {
+        // Métodos são funções dentro das classes
+
+        // CRITÉRIO DE NOMEAÇÃO
+        // Deve ser um verbo e método camelCase
+
+        // No fim, return
+    }
+
+    private void metodoPrivado(){
+        // Método privado
+    }
+}
