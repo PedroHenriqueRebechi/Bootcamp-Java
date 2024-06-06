3c1704df6f1f832ce1d2599c12ab88befe71dfbf
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-06-05 22:06:48 -0300

    Atualização exercício Smart TV

--- Primeiro Projeto Java/src/edu/pedrohenriquerebechi/exercicio/exercicioSmartTV.java
+++ Primeiro Projeto Java/src/edu/pedrohenriquerebechi/exercicio/exercicioSmartTV.java
@@ -1,16 +1,37 @@
 package edu.pedrohenriquerebechi.exercicio;
 
-public class exercicioSmartTV {
-    boolean ligada = false;
-    int canal = 1;
-    int volume=25;
+    public class exercicioSmartTV {
+        boolean ligada = false;
+        int canal = 1;
+        int volume=25;
 
-    public void ligar(){
-        ligada=true;
-    }
-    public void desligar(){
-        ligada=false;
-    }
+        public void ligar(){
+            ligada=true;
+        }
+        public void desligar(){
+            ligada=false;
+        }
+
+        public void aumentarVolume(){
+            volume++;
+        }
+
+        public void diminuirVolume(){
+            volume--;
+        }
+
+        public void aumentarCanal(){
+            canal++;
+        }
+
+        public void diminuirCanal(){
+            canal--;
+        }
+
+        public void mudarCanal (int novoCanal){
+            canal = novoCanal;
+            
+        }
 
 
 }
--- Primeiro Projeto Java/src/edu/pedrohenriquerebechi/exercicio/usuario.java
+++ Primeiro Projeto Java/src/edu/pedrohenriquerebechi/exercicio/usuario.java
@@ -3,14 +3,26 @@ package edu.pedrohenriquerebechi.exercicio;
 public class usuario {
 
     public static void main(String[] args) throws Exception {
-        exercicioSmartTV smartTV = new smartTV();
+        exercicioSmartTV smartTV = new exercicioSmartTV();
 
-        System.out.println("TV ligada? " + smartTV.ligada)
-        System.out.println("Canal atual? " + smartTV.canal)
-        System.out.println("Volume atual? " + smartTV.volume)
+        System.out.println("TV ligada? " + smartTV.ligada);
+        System.out.println("Canal atual? " + smartTV.canal);
+        System.out.println("Volume atual? " + smartTV.volume);
 
         smartTV.ligar();
-        System.out.println("Novo status TV ligada? " + smartTV.ligada)
+        System.out.println("Novo status TV ligada? " + smartTV.ligada);
 
+        smartTV.desligar();
+        System.out.println("Novo status TV desligada: " + smartTV.ligada);
+
+        smartTV.aumentarVolume();
+        System.out.println("Aumenta o volume, agora está: " + smartTV.volume);
+
+        smartTV.diminuirVolume();
+        System.out.println("Diminui o volume, agora está: " + smartTV.volume);
+
+        smartTV.mudarCanal(34);
+        System.out.println("Canal escolhido: " + smartTV.canal);
+        
     }
 }
